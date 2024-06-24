3279618274e235ecf0cb8022248c0359cd4ed8d6
Prithul Paul <86964804+Prithul-Paul@users.noreply.github.com>
2024-06-24 18:11:34 +0000

    conversion of datatype

---
+++ datatype-conversion.js
@@ -0,0 +1,12 @@
+let score = "Prithul"
+
+let numberToString = Number(score)
+// console.log(typeof numberToString);
+// console.log(numberToString);
+
+// let isLoggedIn = "Prithul";
+let isLoggedIn = undefined;
+
+let BooleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(typeof(BooleanIsLoggedIn));
+console.log(BooleanIsLoggedIn);
