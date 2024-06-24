93267bb965036bf2883797ee6bdf0c7b18331b5a
Prithul Paul <86964804+Prithul-Paul@users.noreply.github.com>
2024-06-24 16:01:51 +0000

    variable

---
+++ variable.js
@@ -0,0 +1,6 @@
+const accountId = 1334;
+let accountName = "Prithul Paul";
+var accountEmail = "ptithulpaul@gmail.com";
+//let accountId = 122;
+console.log(accountId);
+console.table([accountId, accountName, accountEmail]);
