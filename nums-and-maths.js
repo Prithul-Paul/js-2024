9c3cd61c86c75138351df1b044ddd043d1929cb9
Prithul Paul <86964804+Prithul-Paul@users.noreply.github.com>
2024-06-26 16:44:01 +0000

    js basics

---
+++ nums-and-maths.js
@@ -0,0 +1,9 @@
+let number = new Number(400)
+let anotherNumber = 24.856
+let hundred = 100000000
+// console.log(typeof number.toString());
+// console.log(number.toFixed(2));
+// console.log(anotherNumber.toPrecision(3));
+console.log(hundred.toLocaleString('en-IN'));
+
+
---
+++ string.js
@@ -0,0 +1,18 @@
+let name = "Prithul Paul";
+let newString = "Hello!!!"
+let url = "https://prithul%20paul.com/";
+let anotherName = "pradip-kumar-paul";
+// let name = new String("    Prithul Paul    ");
+let repoCount = 50;
+
+// console.log(`Your name is ${name} and your repo count is ${repoCount}`);
+// console.log(name.indexOf("l"));
+// console.log(name);
+// console.log(name.trim());
+// console.log(url.replace("%20", ""));
+// console.log(anotherName.split("-"));
+// console.log(name.slice(0, 4));
+// console.log(name.at(-6));
+// console.log(newString.concat(' ', name));
+// console.log(name.endsWith('Paul', 12));
+
