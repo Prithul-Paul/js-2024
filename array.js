8c675d88f7d20b0bbab9568a69de41fad09e6d3a
Prithul Paul <86964804+Prithul-Paul@users.noreply.github.com>
2024-06-26 18:49:36 +0000

    Array

---
+++ array.js
@@ -0,0 +1,21 @@
+let myArr = [0, 1, 2, 3, 4]
+
+let myHeros = new Array("Spider-Man", "Super-Man", "Bat-Man");
+// myHeros.push("Iron-Man");
+// myHeros.pop();
+// myHeros.unshift("Iron-Man");
+// myHeros.shift();
+
+// console.log(myHeros.includes("Super-Man"));
+// console.log(myHeros.indexOf("-Man"));
+
+let newArr = myHeros.join();
+// console.log(typeof myHeros);
+// console.log(typeof newArr);
+
+// let myNewArr = myArr.slice(1, 3); /** Create a new array without modifying the original array */
+// console.log(myNewArr);
+
+// myArr.splice(1, 3); /** Modify the original array by deleting the array elements */
+// console.log(myArr);
+// console.log(myNewArr);
